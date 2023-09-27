b58a744ea87e4d96d7a0a32b86e43ddb07d47bf0
Angelik92 <angelique.parizet@gmail.com>
2023-09-27 06:01:39 +0200

    Create service section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AdsRepository;
+use App\Repository\ServicesRepository;
+use App\Repository\TestimonialsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ServicesRepository $servicesRepository, AdsRepository $adsRepository, TestimonialsRepository $testimonialsRepository): Response
     {
+        $services = $servicesRepository->findAll();
+
+        $ads = $adsRepository->findBy([], ['id' => 'DESC'], 6);
+
+        $testimonials = $testimonialsRepository->findBy(['approved' => 1], ['id' => 'DESC'], 6);
+
         return $this->render('pages/homepage.html.twig', [
-            'controller_name' => 'HomeController',
+            'services' => $services,
+            'ads' => $ads,
+            'testimonials' => $testimonials
         ]);
     }
 }
