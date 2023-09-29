bfc7ce67e2a357b6e127c6a9963266cccf9cdb6d
Angelik92 <angelique.parizet@gmail.com>
2023-09-29 10:05:22 +0200

    Create the basic dashboard template and sidebar

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(): Response
     {
         return $this->render('pages/dashboard.html.twig', [
