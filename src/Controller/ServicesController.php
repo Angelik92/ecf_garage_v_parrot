d78b833ba07fa55f3bd632a006bae03378755f07
Angelik92 <angelique.parizet@gmail.com>
2024-02-10 09:11:03 +0100

    Create the service page rendering

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ServicesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ServicesController extends AbstractController
 {
     #[Route('/services', name: 'services')]
-    public function index(): Response
+    public function index(ServicesRepository $servicesRepository): Response
     {
+        $services = $servicesRepository->findAll();
+
         return $this->render('pages/services/services.html.twig', [
-            'controller_name' => 'ServicesController',
+            'services' => $services,
         ]);
     }
+
 }
